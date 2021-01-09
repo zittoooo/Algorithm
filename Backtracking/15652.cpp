a315d7569398ae993d61ea69fed7c442fdb267e0
이지호 <stellar7484@naver.com>
2021-01-09 19:49:43 +0900

    n과m 4

---
+++ Backtracking/15652.cpp
@@ -0,0 +1,51 @@
+// N과 M (4)
+#include <iostream>
+using namespace std;
+int arr[8];
+bool isused[8];
+int n, m;
+
+void    func(int k)
+{
+    if (k == m)
+    {
+        for (int i = 0; i < m; i++)
+            cout << arr[i] << ' ';
+        cout << '\n';
+        return ;
+    }
+    for (int i = 1; i <= n ; i++)
+    {
+        if (!isused[i] && i >= arr[k-1])
+        {
+            arr[k] = i;
+            func(k+1);
+            isused[i] = 0;
+        }
+    }
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> n >> m;
+    func(0);
+}
+
+/*
+입력
+4 2
+
+출력
+1 1
+1 2
+1 3
+1 4
+2 2
+2 3
+2 4
+3 3
+3 4
+4 4
+*/

88b201cbee51a943736dc585a195d506741ad6fc
이지호 <stellar7484@naver.com>
2021-01-16 16:46:04 +0900

    리모컨

---
+++ BFS/3120_cu.cpp
@@ -0,0 +1,30 @@
+// 리모컨
+#include <iostream>
+#include <queue>
+using namespace std;
+queue<int> Q;
+int dist[100];
+
+int a, b, cnt;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> a >> b;
+    fill(dist, dist + 100, -1);
+    Q.push(a);
+    dist[a] = 0;
+    while (dist[b] == -1)
+    {
+        int cur = Q.front(); Q.pop();
+        for (int nxt : {cur+1, cur-1, cur+5, cur-5, cur+10, cur-10})
+        {
+            if (nxt < 0 || nxt > 100) continue;
+            if (dist[nxt] >= 0) continue;
+            dist[nxt] = dist[cur]+1;
+            Q.push(nxt);
+        }
+    }
+    cout << dist[b];
+}

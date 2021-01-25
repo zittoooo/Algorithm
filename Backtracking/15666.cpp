9227b09e913c708e122346272bdad817d3066816
이지호 <stellar7484@naver.com>
2021-01-25 15:58:15 +0900

    N과M 12

---
+++ Backtracking/15666.cpp
@@ -0,0 +1,42 @@
+// N과 M (12)
+#include <iostream>
+#include <algorithm>
+#include <set>
+using namespace std;
+int N, M;
+bool    isused[10];
+int     arr[10];
+int     input[10];
+
+void    func(int cur)
+{
+    if (cur == M)
+    {
+        for (int i = 0; i < M; i++)
+        {
+            cout << arr[i] << ' ';
+        }
+        cout << '\n';
+        return ;
+    }
+    int before = 0;
+    for (int i = 1; i <= N; i++)
+    {
+        if (!isused[i] && (input[i] != before) && (input[i] >= arr[cur-1] ))
+        {
+            arr[cur] = input[i];
+            before = arr[cur];
+            func(cur+1);
+            isused[i]= 0;
+        }
+    }
+}
+
+int main()
+{
+    cin >> N >> M;
+    for (int i = 1; i <= N; i++)
+        cin >> input[i];
+    sort(input+1, input + N+1);
+    func(0);
+}

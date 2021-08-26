0d364b4982ad66a6547040dd91f4b5a1c8fa1430
jiholee <stellar7484@naver.com>
2021-08-26 21:06:39 +0900

    알파벳 찾기

---
+++ baekjoon/10809.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int alpa[30] = {-1,};
+
+int main()
+{
+    string str;
+    for (int i = 0; i < 30; i++)
+        alpa[i] = -1;
+    cin >> str;
+    for (int i = 0; i < str.length(); i++)
+    {
+        if (alpa[str[i] - 'a'] != -1)
+            continue ;
+        alpa[str[i] - 'a']  = i;
+    }
+
+    for (int i = 0; i < 26; i++)
+        cout << alpa[i] << " ";
+    return 0;
+}

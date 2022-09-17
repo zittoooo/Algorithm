e9e376faacf800bffa5dee5d4061d1051549c1f7
jiholee <stellar7484@naver.com>
2022-09-17 15:39:35 +0900

    Update 10816_2.py

--- binarySearch/10816_2.py
+++ binarySearch/10816_2.py
@@ -29,3 +29,14 @@ def lower_idx(num, n):
 for num in nums:
     print(upper_idx(num, n) - lower_idx(num, n))
 
+####################
+import bisect
+
+n = int(input())
+arr = sorted(list(map(int, input().split())))
+m = int(input())
+nums = list(map(int, input().split()))
+
+for num in nums:
+    # print(bisect.bisect_right(arr, num), bisect.bisect_left(arr, num))
+    print(bisect.bisect_right(arr, num) - bisect.bisect_left(arr, num), end = ' ')

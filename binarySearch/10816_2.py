77abf679e6fd0f4090393d4cc9760323b4104d7c
jiholee <stellar7484@naver.com>
2022-09-17 15:26:19 +0900

    숫자 카드 2

---
+++ binarySearch/10816_2.py
@@ -0,0 +1,31 @@
+n = int(input())
+arr = sorted(list(map(int, input().split())))
+
+m = int(input())
+nums = list(map(int, input().split()))
+
+def upper_idx(num, n):
+    st, ed = 0, n
+    while st < ed:
+        mid = (st + ed) // 2
+        
+        if arr[mid] > num:
+            ed = mid
+        else:
+            st = mid + 1
+    return st
+    
+def lower_idx(num, n):
+    st, ed = 0, n
+    while st < ed:
+        mid = (st + ed) // 2
+        
+        if arr[mid] >= num:
+            ed = mid
+        else:
+            st = mid + 1
+    return st
+
+for num in nums:
+    print(upper_idx(num, n) - lower_idx(num, n))
+
